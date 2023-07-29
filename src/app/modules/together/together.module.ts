d8a80e8c74340776fe708ce80fd81c3ef003ddd7
Marta Zazlak <marta.zazlak@hotmail.com>
2023-07-29 09:32:50 +0200

    refactor

--- src/app/modules/together/together.module.ts
+++ src/app/modules/together/together.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+
 import { MainTitleModule } from 'app/modules/main-title/main-title.module';
 import { SectionWrapperModule } from 'app/modules/section-wrapper/section-wrapper.module';
 import { TogetherComponent } from './together.component';
 
 @NgModule({
     declarations: [TogetherComponent],
-    imports: [CommonModule, MainTitleModule, SectionWrapperModule],
+    imports: [CommonModule, MainTitleModule, SectionWrapperModule, NzTypographyModule],
     exports: [TogetherComponent],
 })
 export class TogetherModule {}
