ae5715271ed91adc7a121f23156df36ca606d397
Marta Zazlak <marta.zazlak@hotmail.com>
2023-07-30 12:07:31 +0200

    see you panel

--- src/app/modules/see-you-panel/see-you-panel.component.ts
+++ src/app/modules/see-you-panel/see-you-panel.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
+import { WithTranslationsComponent } from 'app/components';
 
 @Component({
-  selector: 'app-see-you-panel',
-  templateUrl: './see-you-panel.component.html',
-  styleUrls: ['./see-you-panel.component.less']
+    selector: 'app-see-you-panel',
+    templateUrl: './see-you-panel.component.html',
+    styleUrls: ['./see-you-panel.component.less'],
 })
-export class SeeYouPanelComponent {
-
-}
+export class SeeYouPanelComponent extends WithTranslationsComponent {}
